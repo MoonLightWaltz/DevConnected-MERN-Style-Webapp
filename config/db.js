a74102e983382b6cb21e2022a51f241b3d61423f
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-11-03 16:28:29 -0800

    third commit

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
+//backend/database connection
+
 const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(db);
 
     console.log("MongoDB Connected...");
   } catch (err) {
