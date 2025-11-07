d624cf34c9fb443d883e0c1df49ebf5efd617775
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-11-06 21:15:21 -0800

    sixth commit - implementing jwt jsonwebtoke

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 const User = require("../../config/models/User");
@@ -24,7 +26,7 @@ router.post(
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
@@ -64,7 +66,22 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-      res.send("User registered");
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(500).send("Server error");
