6200146b8fc14df7862735e90916cd781ae8d267
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-11-16 14:07:39 -0800

    Twelfth commit, adding route to delete a profile and user account, created test account via postman, deleted it via the token ID, testing getting all profiles and confirmed it was removed. Verfied through our profile tests on mongo db this was removed as well

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -147,4 +147,22 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    DELETE profile, user & posts
+// @access  Private
+
+router.delete("/", auth, async (req, res) => {
+  try {
+    // @todo - remove users posts
+    //remove profile
+    await Profile.findOneAndDelete({ user: req.user.id });
+    //Remove user
+    await User.findOneAndDelete({ _id: req.user.id });
+    res.json({ msg: "User deleted" });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
