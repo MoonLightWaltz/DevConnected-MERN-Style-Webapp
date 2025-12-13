dd6474c0faef584eba5f0b14479db1c8ba0ba8fe
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-12-12 23:25:00 -0800

    Eighteenth commit, added the ability to get all users posts from most recent. Created a route to get user posts by user ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -23,12 +23,12 @@ router.post(
     try {
       const user = await User.findById(req.user.id).select("-password");
 
-      const newPost = {
+      const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id,
-      };
+      });
 
       const post = await newPost.save();
 
@@ -39,5 +39,41 @@ router.post(
     }
   }
 );
+
+// @route   GET api/posts
+// @desc    GET all posts
+// @access  Private
+
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    GET a single user post by id
+// @access  Private
+
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(400).json({ msg: "post not found" });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(err.message);
+    if (err.kind === "Object.id") {
+      return res.status(400).json({ msg: "post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
