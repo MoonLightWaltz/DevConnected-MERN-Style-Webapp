b2e4f9632a4f786c2efbc3ccc177122959d93745
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-12-23 21:06:31 -0800

    Twentythird commit, added the Landing page and Navbar components and linked them to our app.js file. Ensured we could navigate to each one by clicking the links via the application. Ensured, a tags were changed to Link and href were changed to 'to' adjusted the paths as well.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,39 @@ import "./App.css";
 import React from "react";
 import Navbar from "./Components/Layout/Navbar";
 import Landing from "./Components/Layout/Landing";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Layout/auth/Login";
+import Register from "./Components/Layout/auth/Register";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Landing />
+      {" "}
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Landing />} />
+
+          <Route
+            path="/register"
+            element={
+              <section className="container">
+                <Register />
+              </section>
+            }
+          />
+
+          <Route
+            path="/login"
+            element={
+              <section className="container">
+                <Login />
+              </section>
+            }
+          />
+        </Routes>
+      </Router>
     </>
   );
 };
