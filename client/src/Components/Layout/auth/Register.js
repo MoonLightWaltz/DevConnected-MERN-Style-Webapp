02ad8822e9188fbda8197bcd144bfc54119d78ba
MoonLightWaltz <kyle.bushnell@hotmail.com>
2025-12-29 15:58:34 -0800

    TwentyFifth commit, ensured when adding user info via the form that it adds it to the database using axios, removed that for now as I will be bringing in reduxtomorrow. Also added the login screen, made it similar to the register form but just removed some things, did some additional clean up and bug fixes as well

--- client/src/Components/Layout/auth/Register.js
+++ client/src/Components/Layout/auth/Register.js
@@ -1,6 +1,7 @@
 import { set } from "mongoose";
 import React from "react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -15,12 +16,12 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
       console.log("Passwords do not match");
     } else {
-      console.log(formData);
+      console.log("SUCESS");
     }
   };
 
@@ -80,7 +81,7 @@ const Register = () => {
         <input type="submit" className="btn btn-primary" value="Register" />
       </form>
       <p className="my-1">
-        Already have an account? <a href="login.html">Sign In</a>
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </>
   );
