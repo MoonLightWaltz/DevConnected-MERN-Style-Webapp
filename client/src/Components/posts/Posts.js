4a023eeb02d75af46e38a0039d98893728af46ec
MoonLightWaltz <kyle.bushnell@hotmail.com>
2026-02-16 16:35:12 -0800

    Forty-Ninth commit, added user posts to be displayed in the post tab. Ensured nothing was hard coded and we were pulling the data from the user post object. Added a PostItem.js component we are displaying on our web page. Added the post content, name of the user, comment, likes, dislikes and date of the post

--- client/src/Components/posts/Posts.js
+++ client/src/Components/posts/Posts.js
@@ -3,12 +3,28 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPosts } from "../../action/post";
 import Spinner from "../Layout/Spinner";
+import PostItem from "./PostItem";
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>Posts</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i>Welcome to the community
+      </p>
+      {/* PostForm */}
+      <div className="posts">
+        {posts.map((post) => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
